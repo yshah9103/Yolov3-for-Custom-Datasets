b8a80116243fdfd8906f2d34aa45035746e3392e
Jash Mehta <81267080+jashmehta20@users.noreply.github.com>
2021-10-05 10:55:37 -0400

    Initial Commit

---
+++ Person Detection /yolo.py
@@ -0,0 +1,63 @@
+import cv2
+import numpy as np
+
+cap=cv2.VideoCapture(1)
+whT=320
+classesFiles='classes.names'
+classNames=[]
+
+confThreshold=0.5
+nms_threshold=0.3
+
+with open(classesFiles,'rt') as f:
+    classNames=f.read().rstrip('\n').split('\n')
+
+modelConfiguration='yolov3_custom.cfg'
+modelWeights='yolov3_custom_final.weights'
+
+net=cv2.dnn.readNetFromDarknet(modelConfiguration,modelWeights)
+net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
+net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
+layerNames=net.getLayerNames()
+outputNames = [layerNames[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+def findObjects(outputs,img):
+    hT,wT,cT=img.shape
+    bbox=[]
+    classIds=[]
+    confs=[]
+
+    for output in outputs:
+        for det in output:
+            scores=det[5:]
+            classId=np.argmax(scores)
+            confidence=scores[classId]
+            if confidence>confThreshold:
+                w,h=int(det[2]*wT),int(det[3]*hT)
+                x,y=int((det[0]*wT) - w/2),int((det[1]*hT)-h/2)
+                bbox.append([x,y,w,h])
+                classIds.append(classId)
+                confs.append(float(confidence))
+    indices = cv2.dnn.NMSBoxes(bbox, confs, confThreshold, nms_threshold)
+    colors = np.random.uniform(0,255,size =(len(bbox),3))
+    for i in indices:
+        i=i[0]
+        box=bbox[i]
+        color=colors[i]
+        x,y,w,h=box[0],box[1],box[2],box[3]
+        cv2.rectangle(img, (x,y),(x+w,y+h) , color,2)
+        cv2.putText(img,f'{classNames[classIds[i]].upper()} {int(confs[i]*100)}%',(x,y-10),cv2.FONT_HERSHEY_SIMPLEX,0.6,color,2)
+
+while True:
+    success,img=cap.read()
+    blob=cv2.dnn.blobFromImage(img,1/255,(whT,whT),[0,0,0],1,crop=False)
+    net.setInput(blob)
+
+    outputs=net.forward(outputNames)
+
+    findObjects(outputs, img)
+    cv2.imshow("image", img)
+
+    key=cv2.waitKey(1)
+    if key == 27:
+        break
